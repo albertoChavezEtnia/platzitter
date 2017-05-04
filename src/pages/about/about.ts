69c331537f7edd01cdc09941193bc709b243ae1d
Esteban r <esteban.ramos.f@gmail.com>
2017-05-04 03:33:08 -0300

    update - adding components in notifications

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,21 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  notifications:Array<Notifications> = [
+      {action: 'reply', img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@freddier', text:'platzitter es lo mas'},
+      {action: 'replatzitt', img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@cvander', text:'que chulo que esta esta app en iOS'},
+      {action: 'mention', img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'}
+    ];
+
   constructor(public navCtrl: NavController) {
 
   }
 
+}
+
+interface Notifications{
+  img:string;
+  nick_name:string;
+  text:string;
+  action:string;
 }
