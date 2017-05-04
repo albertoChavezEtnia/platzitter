670c1b3d325669a6d62e18577573bf54ca809e05
Esteban r <esteban.ramos.f@gmail.com>
2017-05-04 03:52:49 -0300

    update - adding components in profile

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -7,8 +7,59 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  me:Me = {
+    "img":'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg',
+     nick_name:'@jorgeucano',
+    "interactions":1,
+    "github": "jorgeucano",
+    "twitter": "jorgeucano",
+    "medium": "jorgeucano",
+    "bio": "Jorge Cano es Google Developer Expert en Angular y Tecnologías Web. Organizador de ngbaires. Director de tecnologias en JavaScript es especialista en Angular y Firebase. Tambien da cursos, charlas y escribe articulos tecnicos."
+  };
 
+
+  listado:Array<twitt> = [
+    {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+    {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+    {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+    {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+    {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+    {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+    {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'},
+    {img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'}
+  ];
+
+  notifications:Array<Notifications> = [
+    {action: 'reply', img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@freddier', text:'platzitter es lo mas'},
+    {action: 'replatzitt', img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@cvander', text:'que chulo que esta esta app en iOS'},
+    {action: 'mention', img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'}
+  ];
+
+  constructor(public navCtrl: NavController) {
+    this.me.interactions = this.listado.length;
   }
 
+}
+
+interface Me{
+  img:string;
+  nick_name:string;
+  interactions:number;
+  github:string;
+  twitter:string;
+  medium:string;
+  bio:string;
+}
+
+interface twitt{
+  img:string;
+  nick_name:string;
+  text:string;
+}
+
+interface Notifications{
+  img:string;
+  nick_name:string;
+  text:string;
+  action:string;
 }
