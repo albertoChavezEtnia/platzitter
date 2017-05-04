1ba78e5a9042b1d3eecda1b4c30fb59b346df69a
Esteban r <esteban.ramos.f@gmail.com>
2017-05-04 03:20:24 -0300

    update - adding ionic components in home view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+	listado:Array<tweet> = [
+		{img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@freddier', text:'platzitter es lo mas'},
+		{img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@cvander', text:'que chulo que esta esta app en iOS'},
+		{img:'https://pbs.twimg.com/profile_images/703539825853464577/jppQNmXD_400x400.jpg', nick_name:'@jorgeucano', text:'test 1234'}
+	];
   constructor(public navCtrl: NavController) {
 
   }
 
+}
+
+interface tweet{
+	img:string;
+	nick_name:string;
+	text:string;
 }
